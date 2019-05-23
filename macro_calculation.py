81a135b09de9ec0f3082c954fe5c0333ead014ed
boniverski <bosko.rabrenovic@gmail.com>
2019-05-23 20:28:38 +0200

    macro data transform

--- macro_calculation.py
+++ macro_calculation.py
@@ -4,21 +4,42 @@ from scipy import stats
 import sklearn
 import math
 
-default_rate = pd.read_excel("/home/bosko/Desktop/macro/banks_europe.xlsx")
-default_rate.rename(columns={'bad_rate': 'DR'}, inplace=True)
-default_rate.loc[default_rate.DR == 0, 'DR'] = 0.00003
-default_rate.loc[:, 'DR_TRANSFORMED'] = stats.norm.ppf(default_rate.DR)
-default_rate.loc[:, 'DR_mean'] = default_rate.DR.mean()
-default_rate.loc[:, 'DR_TRANSFORMED_mean'] = default_rate.DR_TRANSFORMED.mean()
-default_rate.loc[:, 'DR_TRANSFORMED_var'] = np.var(default_rate.DR_TRANSFORMED, ddof=1)
+#Importing default rate data
+output = pd.read_excel("/home/bosko/Desktop/model-calculation/banks_europe.xlsx")
 
-dr_trans_v = default_rate.DR_TRANSFORMED_var
-default_rate.loc[:, 'rho_derived'] = dr_trans_v/(dr_trans_v+1)
+#Importing macroeconomic data
+macro_data = pd.read_excel("/home/bosko/Desktop/model-calculation/macro_data.xlsx")
 
-dr_m_qnorm = stats.norm.ppf(default_rate.DR_mean)
-dr_qnorm = default_rate.DR_TRANSFORMED
-rho = default_rate.rho_derived
+#Default rate transformation
+output.rename(columns={'bad_rate': 'DR'}, inplace=True)
+output.loc[output.DR == 0, 'DR'] = 0.00003
+output['DR_TRANSFORMED'] = stats.norm.ppf(output.DR)
+output['DR_mean'] = output.DR.mean()
+output['DR_TRANSFORMED_m'] = output.DR_TRANSFORMED.mean()
+output['DR_TRANSFORMED_v'] = np.var(output.DR_TRANSFORMED, ddof=1)
+dr_trans_v = output.DR_TRANSFORMED_v
+output['rho'] = dr_trans_v/(dr_trans_v+1)
 
-default_rate.loc[:, 'z_5'] = (dr_m_qnorm-np.sqrt(1-rho)*dr_qnorm)/np.sqrt(rho)
+#System risk factor calculation
+dr_m_trans = stats.norm.ppf(output.DR_mean)
+dr_trans = output.DR_TRANSFORMED
+rho = output.rho
+output['Z'] = (dr_m_trans-np.sqrt(1-rho)*dr_trans)/np.sqrt(rho)
 
-print(default_rate)
+#Transforming absolute macroeconomic variables: GDP and Property
+
+GDP = macro_data.GDP
+GDP_lag = macro_data.GDP.shift(1)
+Property = macro_data.Property
+Property_lag = macro_data.Property.shift(1)
+
+macro_data['GDP_YoY'] = 100 * (GDP - GDP_lag)/GDP_lag
+macro_data['Property_YoY'] = 100 * (Property - Property_lag)/Property_lag
+macro_data.drop(columns=['GDP', 'Property'], inplace=True)
+macro_data.rename(columns={'GDP_YoY': 'GDP', 'Property_YoY': 'Property'}, inplace=True)
+
+for column in macro_data:
+    if column != 'year':
+        macro_data[column + '1']= macro_data[column].shift(1)
+
+print(macro_data)
